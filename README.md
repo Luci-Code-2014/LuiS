ba82a7e92e3526f18e72801c6a1285704abf93c4
Luci-Code-2014 <3967742419@qq.com>
2026-08-25 16:51:03 +0800

    更改为 64 位长模式 | changed 64 bits - long mode

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # LuiS
-Luci (me) make oparting system
 
 其实这个仓库只有代码, ISO 还没做呢
+等做好 ISO 了我发到 Releases
+提示: 编译用 NASM
--- kernel_entry.asm
+++ kernel_entry.asm
@@ -1,9 +1,9 @@
-[bits 32] ; 32 位
+[bits 64] ; 64 位
 [extern _kernel_main] ; 有个叫 kernel_main 的函数在另一个文件内
 
 global _start ; 全局: _start
 _start: ; 程序入口点
-    mov esp, 0x90000 ; 给 kernel.c 一个栈
+    mov rsp, 0x90000 ; 给 kernel.c 一个栈
     call _kernel_main ; 调用 kernel.c 里面 kernel_main 函数
     cli ; clear interrupt
     hlt ; halt
